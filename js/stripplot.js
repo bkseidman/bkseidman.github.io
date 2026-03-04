949e370b6db30708b5ece04f7fc465b2df0d3db3
bkseidman <143037030+bkseidman@users.noreply.github.com>
2026-03-03 18:52:39 -0800

    Update stripplot.js

--- js/stripplot.js
+++ js/stripplot.js
@@ -6,7 +6,7 @@ let values = [];
 let points = [];
 let minV, maxV;
 
-let m = { l: 80, r: 30, t: 80, b: 80 };
+let m = { l: 90, r: 40, t: 90, b: 90 };
 
 function preload() {
   table = loadTable(CSV_FILE, "csv", "header");
@@ -49,34 +49,51 @@ function setup() {
 function drawGrid(x0, y0, x1, y1) {
   stroke(0, 20);
   strokeWeight(1);
+
   for (let i = 0; i <= 10; i++) {
     const y = lerp(y0, y1, i / 10);
     line(x0, y, x1, y);
   }
+
   for (let i = 0; i <= 10; i++) {
     const x = lerp(x0, x1, i / 10);
     line(x, y0, x, y1);
   }
 }
 
-function drawAxes(x0, y0, x1, y1) {
+function drawYAxisTicks(x0, y0, y1) {
+  fill(0);
+  noStroke();
+  textSize(14);
+  textAlign(RIGHT, CENTER);
+
+  for (let i = 0; i <= 7; i++) {
+    const val = lerp(minV, maxV, i / 7);
+    const y = map(val, minV, maxV, y1, y0);
+    text(val.toFixed(1), x0 - 12, y);
+  }
+}
+
+function drawAxesAndLabels(x0, y0, x1, y1) {
   stroke(0);
   strokeWeight(3);
+
   line(x0, y1, x1, y1);
   line(x0, y0, x0, y1);
 
   const cx = (x0 + x1) / 2;
   line(cx, y1, cx, y1 + 12);
 
-  noStroke();
   fill(0);
-  textSize(28);
+  noStroke();
+
   textAlign(CENTER, TOP);
-  text("Strip Plot (p5)", width / 2, 20);
+  textSize(26);
+  text("Strip Plot (p5)", width / 2, 25);
 
   textSize(18);
   push();
-  translate(x0 - 60, (y0 + y1) / 2);
+  translate(x0 - 65, (y0 + y1) / 2);
   rotate(-HALF_PI);
   textAlign(CENTER, TOP);
   text(STRIP_VAR, 0, 0);
@@ -92,7 +109,8 @@ function draw() {
   const y1 = height - m.b;
 
   drawGrid(x0, y0, x1, y1);
-  drawAxes(x0, y0, x1, y1);
+  drawAxesAndLabels(x0, y0, x1, y1);
+  drawYAxisTicks(x0, y0, y1);
 
   noStroke();
   fill(31, 119, 180, 220);
